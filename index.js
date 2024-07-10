17c546cec3e2171f36fdc6bf1af2e1a5bbf95cdb
SUPA93 <supaflavaz93@hotmail.com>
2024-07-10 14:25:06 +0200

    added style and function for timer

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 let totalSeconds;
+let interval;
 
-function countDown(){
-    console.log(totalSeconds);
+function countDown() {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    const sec = seconds < 10 ? "0" + seconds : seconds;
+
+    countdownDisplay.textContent = `${minutes}:${sec}`;
+    
+    if(totalSeconds > 0 ) {
+        totalSeconds--;
+
+    }else {
+        countdownDisplay.textContent = "Terminé!";
+        clearInterval(interval);
+    }
 }
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
-
+    
     if(isNaN(choice.value)) {
         alert("Merci d'entrer un chiffre");
     } else{
         totalSeconds = choice.value * 60;
         choice.value = "";
-        setInterval(countDown, 1000);
+        clearInterval(interval);
+        interval = setInterval(countDown, 1000);
     }
 })
 
 
 
-// Créer un événement à la validation du form pour lancer le compte à rebours
+
